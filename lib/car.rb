dbd41e2cf8bd68205f23a3c2e88b3d5680fa500c
senadakadric <skadric@fordham.edu>
2020-05-16 12:35:34 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,10 @@
 require_relative "./vehicle.rb"
+
+#we use "<" to allow the Car class to inherit what is in the Vehicle class
+class Car < Vehicle
+
+    def go
+        return 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!' 
+    end
+
+end
