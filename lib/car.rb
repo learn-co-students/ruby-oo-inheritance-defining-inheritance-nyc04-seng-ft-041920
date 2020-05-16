add8788e9b9b9f575a15874de79776af1ff19279
senadakadric <skadric@fordham.edu>
2020-05-16 12:36:51 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,8 +1,9 @@
 require_relative "./vehicle.rb"
 
-#we use "<" to allow the Car class to inherit what is in the Vehicle class
+#we use "<" to allow the Car class to inherit all of the methods in the Vehicle class
 class Car < Vehicle
 
+    #even though the parent vehicle class has its own go instance method, if you write one in this class it will overwrite the vehicle method, and the class will use whats here
     def go
         return 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!' 
     end
